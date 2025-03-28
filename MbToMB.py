d3415fec824fdeafbc959cf6dbffa2e5702e7215
Mikhail Tsymbalov <tsymbalov1990@gmail.com>
2025-03-28 10:39:32 +0300

    Dobavil MbToMB and Phrases

---
+++ MbToMB.py
@@ -0,0 +1,3 @@
+znachenie_bites = float(input ('Значение в байтах - '))
+znachenie_megabites = (znachenie_bites / 1048576)
+print (f'Объём данного файла в МБ равен {znachenie_megabites} Mb') 
---
+++ Phrases.py
@@ -0,0 +1,4 @@
+long_phrase = (input ('Длинная фраза - '))
+short_phrase = (input ('Короткая фраза - '))
+result = len(long_phrase) > len(short_phrase)
+print (result)
